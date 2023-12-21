869d3b332ed4c2f6385fb49bc8558ba16bc8475f
Aleksandr Liubenkov <lubenkov.a@gmail.com>
2023-12-21 14:23:29 +0300

    Add output to lerna

--- docker/run.sh
+++ docker/run.sh
@@ -3,7 +3,7 @@
 # Run tests for each package with a pause
 for PACKAGE in $(lerna la -p --json | jq -r '.[].name'); do
   echo "Running command for $PACKAGE"
-  lerna run test --scope="$PACKAGE"
+  lerna run test --scope="$PACKAGE" --stream
 
   sleep 1
 done
