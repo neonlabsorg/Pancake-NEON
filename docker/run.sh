77bc2de2a9ef80b8e01be63e5aac6e177a681327
Aleksandr Liubenkov <lubenkov.a@gmail.com>
2024-04-08 13:56:27 +0700

    Run projects tests directly via runner script

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-# Run tests for each package with a pause
-for PACKAGE in $(lerna la -p --json | jq -r '.[].name'); do
-  echo "Running command for $PACKAGE"
-  lerna run test --scope="$PACKAGE" --stream
+subprojects=($(lerna la -p --json | jq -r '.[].name'))
+error_occurred=0
 
-  sleep 1
+for subproject in "${subprojects[@]}"
+do
+  project_dir="projects/$subproject"
+  if test -f "$project_dir/hardhat.config.ts"; then
+    echo "Running subproject: $subproject"
+    npx hardhat test --config "$project_dir/hardhat.config.ts"
+  fi
+
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    echo "Subproject $subproject failed with exit code $exit_code"
+    error_occurred=1
+  fi
 done
+
+if [ $error_occurred -ne 0 ]; then
+  echo "One or more subprojects failed."
+  exit 1
+fi
+
+echo "All subprojects completed successfully."
+exit 0
